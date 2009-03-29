ad0ee1bdb364e1abb39ac6f93d97bd16a2c22a28
Jordi Bunster <jordi@bunster.org>
2009-03-29 00:20:55 -0400

    Striving for more descriptive variable names

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -26,18 +26,18 @@ class Test::Unit::TestCase
   require File.join(File.dirname(__FILE__), 'fixtures', 'bars_controller')
   require File.join(File.dirname(__FILE__), 'fixtures', 'stuff_controller')
 
-  def assert_activation(tab, *other_tab)
+  def assert_activation(tab_literal, *other_tab_literal)
 
-    tab, other_tab = Tab.new(tab), Tab.new(other_tab)
+    tab, other_tab = Tab.new(tab_literal), Tab.new(other_tab_literal)
 
     assert_block "#{tab.inspect} does not activate #{other_tab.inspect}" do
       tab.activates? other_tab
     end
   end
 
-  def assert_no_activation(tab, *other_tab)
+  def assert_no_activation(tab_literal, *other_tab_literal)
 
-    tab, other_tab = Tab.new(tab), Tab.new(other_tab)
+    tab, other_tab = Tab.new(tab_literal), Tab.new(other_tab_literal)
 
     assert_block "#{tab.inspect} activates #{other_tab.inspect}" do
       not tab.activates? other_tab
